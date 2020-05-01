f849a3f7a885ad3cbaed9db9326b631eb331ea16
cs91chris <cs91chris@voidbrain.me>
2020-05-01 17:08:56 +0200

    fixed issue on setup

--- setup.py
+++ setup.py
@@ -2,30 +2,85 @@
 Flask-CloudflareRemote
 ---------------------
 """
+import os
+import re
+import sys
+
+from setuptools.command.test import test
 from setuptools import setup, find_packages
 
-from flask_cloudflare_remote import author, __version__
+BASE_PATH = os.path.dirname(__file__)
+VERSION_FILE = os.path.join('flask_cloudflare_remote', 'version.py')
 
-with open("README.rst") as fh:
-    long_description = fh.read()
+
+def read(file):
+    """
+
+    :param file:
+    :return:
+    """
+    with open(os.path.join(BASE_PATH, file)) as f:
+        return f.read()
+
+
+def grep(file, name):
+    """
+
+    :param file:
+    :param name:
+    :return:
+    """
+    pattern = r"{attr}\W*=\W*'([^']+)'".format(attr=name)
+    value, = re.findall(pattern, read(file))
+    return value
+
+
+def readme(file):
+    """
+
+    :param file:
+    :return:
+    """
+    try:
+        return read(file)
+    except OSError as exc:
+        print(str(exc), file=sys.stderr)
+
+
+class PyTest(test):
+    def finalize_options(self):
+        """
+
+        """
+        test.finalize_options(self)
+
+    def run_tests(self):
+        """
+
+        """
+        import pytest
+        sys.exit(pytest.main(['tests']))
+
 
 setup(
-    name='Flask-CloudflareRemote',
-    version=__version__,
-    url='https://github.com/cs91chris/flask_cloudflare_remote',
     license='MIT',
-    author=author['name'],
-    author_email=author['email'],
+    name='Flask-CloudflareRemote',
+    url='https://github.com/cs91chris/flask_cloudflare_remote',
+    version=grep(VERSION_FILE, '__version__'),
+    author=grep(VERSION_FILE, '__author_name__'),
+    author_email=grep(VERSION_FILE, '__author_email__'),
     description='Cloudflare proxy support for flask application',
-    long_description=long_description,
+    long_description=readme('README.rst'),
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     install_requires=[
         'Flask',
-        'netaddr==0.*',
+        'netaddr >= 0',
     ],
+    cmdclass={'test': PyTest},
+    test_suite='tests',
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
